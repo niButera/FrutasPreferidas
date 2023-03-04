3e1c2c1b3b7fc2b8ec51295da3adabd351383071
niButera <nikolasxp683@gmail.com>
2023-03-03 22:02:51 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Frutas Preferidas");
+Console.WriteLine();
+Console.WriteLine("1 - Tomate");
+Console.WriteLine("2 - Melancia");
+Console.WriteLine("3 - Maça");
+Console.WriteLine("4 - Laranja");
+Console.WriteLine("5 - Uva");
